ae9981303e76ef2faac5384180be0a2fc2670d5e
MIZiper <miziper@163.com>
2025-03-19 22:09:58 +0100

    remove podman manipulation
    the idea was: a router container instance is able to start analysis session as new container so the environment can be isolated.
    router and analysis instances share same image

--- backend/router_handler.py
+++ backend/router_handler.py
@@ -5,8 +5,6 @@ from datetime import datetime
 
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS # to be removed in final container
-from podman import PodmanClient
-from podman.errors import NotFound, APIError
 import subprocess, requests, json
 
 
@@ -14,9 +12,6 @@ import subprocess, requests, json
 app = Flask(__name__, static_folder="../frontend/dist/")
 CORS(app)
 app_entry = "backend/app_entry.py"
-
-podman_url = 'unix:///run/podman/podman.sock'
-container_image = "dac_web:latest"
 
 SESSID_KEY = "dac-sess_id"
 PROJDIR_KEY = "project_folder"
@@ -48,171 +43,123 @@ user_manager = UserManager()
 
 
 
-with PodmanClient(base_url=podman_url) as client:
-    @app.route("/")
-    @app.route('/projects/<string:project_id>', methods=['GET']) # let frontend to load and judge
-    def index(project_id: str=None):
-        return send_from_directory(app.static_folder, "index.html")
+@app.route("/")
+@app.route('/projects/<string:project_id>', methods=['GET']) # let frontend to load and judge
+def index(project_id: str=None):
+    return send_from_directory(app.static_folder, "index.html")
+
+@app.route("/<path:filename>")
+def static_files(filename):
+    # should let analysis container to serve static files?
+    # so the dependencies can be maintained by container versions
+    return send_from_directory(app.static_folder, filename)
+
+@app.route('/load', methods=['POST'])
+def load_project(project_id: str=None):
+    if project_id is None:
+        project_id = request.get_json().get("project_id")
+
+    project_fpath = path.join(app.config[PROJDIR_KEY], project_id)
+    if path.isfile(project_fpath):
+        with open(project_fpath, mode='r') as fp:
+            config = json.load(fp)['dac']
+
+        sess_id = start_process_session()
+        conn = user_manager.get_sess_conn(sess_id)
+        resp = requests.post(f"http://{conn}/init", json=config)
+        if resp.status_code == 200:
+            return jsonify({"message": "Project loaded", SESSID_KEY: sess_id, "project_id": project_id}), 200
+        else:
+            return jsonify({"error": "Project load failed"}), 500
+    else:
+        return jsonify({"error": "Project not found"}), 404
     
-    @app.route("/<path:filename>")
-    def static_files(filename):
-        # should let analysis container to serve static files?
-        # so the dependencies can be maintained by container versions
-        return send_from_directory(app.static_folder, filename)
+@app.route('/load_saved', methods=['POST'])
+def load_saved_project():
+    project_id = get_project_id_by_path(request.get_json().get("project_path"))
+    return load_project(project_id)
 
-    @app.route('/load', methods=['POST'])
-    def load_project(project_id: str=None):
-        if project_id is None:
-            project_id = request.get_json().get("project_id")
+@app.route("/new", methods=['POST'])
+def new_process_session():
+    sess_id = start_process_session()
+    return jsonify({"message": "Analysis started", SESSID_KEY: sess_id}), 200
+    
+@app.route('/term', methods=['POST'])
+def terminate_process_session():
+    sess_id = request.headers.get(SESSID_KEY)
+    if not user_manager.validate_sess(sess_id):
+        return jsonify({"error": "Invalid or missing session ID"}), 401
+    
+    try:
+        user_manager.get_sess_obj(sess_id).terminate()
+        user_manager.remove_sess(sess_id)
+        return jsonify({"message": "Analysis terminated"}), 200
+    except Exception as e:
+        return jsonify({"error": f"Failed to terminate analysis: {str(e)}"}), 500
+    
+@app.route('/save', methods=['POST'])
+def save_project():
+    sess_id = request.headers.get(SESSID_KEY)
+    if not user_manager.validate_sess(sess_id):
+        return jsonify({"error": "Invalid or missing session ID"}), 401
+    
+    data = request.get_json()
+    project_id = data.get("project_id", "").strip("./")
+    publish_name  = data.get("publish_name", "").strip("./")
+    signature = data.get("signature")
 
+    dac_web_config = {
+        "signature": signature,
+        "version": __VERSION__,
+    }
+
+    if project_id: # not new
         project_fpath = path.join(app.config[PROJDIR_KEY], project_id)
         if path.isfile(project_fpath):
             with open(project_fpath, mode='r') as fp:
-                config = json.load(fp)['dac']
+                config = json.load(fp)
+            config_web = config.get("dac_web", {})
+            if "signature" not in config_web or config_web["signature"]!=signature:
+                dac_web_config["inherit"] = project_id
+                project_id = uuid4().hex
+    else:
+        project_id = uuid4().hex
 
-            sess_id = start_process_session()
-            conn = user_manager.get_sess_conn(sess_id)
-            resp = requests.post(f"http://{conn}/init", json=config)
-            if resp.status_code == 200:
-                return jsonify({"message": "Project loaded", SESSID_KEY: sess_id, "project_id": project_id}), 200
-            else:
-                return jsonify({"error": "Project load failed"}), 500
-        else:
-            return jsonify({"error": "Project not found"}), 404
-        
-    @app.route('/load_saved', methods=['POST'])
-    def load_saved_project():
-        project_id = get_project_id_by_path(request.get_json().get("project_path"))
-        return load_project(project_id)
+    conn = user_manager.get_sess_conn(sess_id)
+    resp = requests.get(f"http://{conn}/save")
+    if resp.status_code == 200:
+        project_fpath = path.join(app.config[PROJDIR_KEY], project_id)
+        config = resp.json()
 
-    @app.route('/_new_', methods=['POST'])
-    def new_container_session():
-        sess_id = uuid4().hex
+        with open(project_fpath, mode="w") as fp:
+            json.dump({
+                "dac": config["config"],
+                "dac_web": dac_web_config,
+            }, fp, indent=2)
 
-        # # Define the path for the progress file or a shared volume
-        # # This example uses a file-based approach for simplicity
-        # progress_file_path = f"/path/to/progress_files/progress_{user_id}.txt"
+        if publish_name:
+            save_fpath = path.join(app.config[SAVEDIR_KEY], publish_name)
+            lines = ""
+            if path.isfile(save_fpath):
+                with open(save_fpath, mode="r") as fp:
+                    lines = fp.read()
 
-        # # Define container volumes (shared storage between containers)
-        # volumes = {"/path/to/progress_files": {"bind": "/path/to/container/progress_files", "mode": "rw"}}
+            with open(save_fpath, mode="w") as fp:
+                line = f"{project_id}; {datetime.now()}; {signature}\n"
+                fp.write(line)
+                fp.write(lines)
 
-        # Start a new container for the analysis module
-        try:
-            container = client.containers.run(
-                container_image,
-                ["python", app_entry],
-                name=f"dac_{sess_id}", # volumes=volumes, bind port
-                remove=True, detach=True
-            )
-            user_manager.set_sess(sess_id, "conn", container.id)
-
-            # TODO: and return project_id?
-            return jsonify({"message": "Analysis started", SESSID_KEY: sess_id}), 200
-        except APIError:
-            pass
-        except Exception as e:
-            return jsonify({"error": f"Failed to start analysis: {str(e)}"}), 500
+        return jsonify({"message": "Project saved", "project_id": project_id}), 200
+    else:
+        return jsonify({"error": "Project save failed"}), 500
     
-    @app.route("/new", methods=['POST'])
-    def new_process_session():
-        sess_id = start_process_session()
-        return jsonify({"message": "Analysis started", SESSID_KEY: sess_id}), 200
+@app.route("/project_files", methods=['POST'])
+def get_project_files():
+    relpath = request.get_json().get("relpath").strip("./")
+    node_dir = path.join(app.config[SAVEDIR_KEY], relpath)
 
-    @app.route('/_term_', methods=['POST'])
-    def terminate_container_session():
-        sess_id = request.headers.get(SESSID_KEY)
-        if not user_manager.validate_sess(sess_id):
-            return jsonify({"error": "Invalid or missing session ID"}), 401
-
-        try:
-            container = client.containers.get(user_manager.get_sess_obj(sess_id))
-            container.remove(force=True)
-            user_manager.remove_sess(sess_id)
-
-            return jsonify({"message": "Analysis terminated"}), 200
-        except NotFound:
-            return jsonify({"error": "Container not found"}), 404
-        except Exception as e:
-            return jsonify({"error": f"Failed to terminate analysis: {str(e)}"}), 500
-        
-    @app.route('/term', methods=['POST'])
-    def terminate_process_session():
-        sess_id = request.headers.get(SESSID_KEY)
-        if not user_manager.validate_sess(sess_id):
-            return jsonify({"error": "Invalid or missing session ID"}), 401
-        
-        try:
-            user_manager.get_sess_obj(sess_id).terminate()
-            user_manager.remove_sess(sess_id)
-            return jsonify({"message": "Analysis terminated"}), 200
-        except Exception as e:
-            return jsonify({"error": f"Failed to terminate analysis: {str(e)}"}), 500
-        
-    @app.route('/save', methods=['POST'])
-    def save_project():
-        sess_id = request.headers.get(SESSID_KEY)
-        if not user_manager.validate_sess(sess_id):
-            return jsonify({"error": "Invalid or missing session ID"}), 401
-        
-        data = request.get_json()
-        project_id = data.get("project_id", "").strip("./")
-        publish_name  = data.get("publish_name", "").strip("./")
-        signature = data.get("signature")
-
-        dac_web_config = {
-            "signature": signature,
-            "version": __VERSION__,
-        }
-
-        if project_id: # not new
-            project_fpath = path.join(app.config[PROJDIR_KEY], project_id)
-            if path.isfile(project_fpath):
-                with open(project_fpath, mode='r') as fp:
-                    config = json.load(fp)
-                config_web = config.get("dac_web", {})
-                if "signature" not in config_web or config_web["signature"]!=signature:
-                    dac_web_config["inherit"] = project_id
-                    project_id = uuid4().hex
-        else:
-            project_id = uuid4().hex
-
-        conn = user_manager.get_sess_conn(sess_id)
-        resp = requests.get(f"http://{conn}/save")
-        if resp.status_code == 200:
-            project_fpath = path.join(app.config[PROJDIR_KEY], project_id)
-            config = resp.json()
-
-            with open(project_fpath, mode="w") as fp:
-                json.dump({
-                    "dac": config["config"],
-                    "dac_web": dac_web_config,
-                }, fp, indent=2)
-
-            if publish_name:
-                save_fpath = path.join(app.config[SAVEDIR_KEY], publish_name)
-                lines = ""
-                if path.isfile(save_fpath):
-                    with open(save_fpath, mode="r") as fp:
-                        lines = fp.read()
-
-                with open(save_fpath, mode="w") as fp:
-                    line = f"{project_id}; {datetime.now()}; {signature}\n"
-                    fp.write(line)
-                    fp.write(lines)
-
-            return jsonify({"message": "Project saved", "project_id": project_id}), 200
-        else:
-            return jsonify({"error": "Project save failed"}), 500
-
-
-        
-    @app.route("/project_files", methods=['POST'])
-    def get_project_files():
-        relpath = request.get_json().get("relpath").strip("./")
-        node_dir = path.join(app.config[SAVEDIR_KEY], relpath)
-
-        dirpath, dirnames, filenames = next(os.walk(node_dir))
-        return jsonify({"dirnames": dirnames, "filenames": filenames}), 200
+    dirpath, dirnames, filenames = next(os.walk(node_dir))
+    return jsonify({"dirnames": dirnames, "filenames": filenames}), 200
 
 
 
