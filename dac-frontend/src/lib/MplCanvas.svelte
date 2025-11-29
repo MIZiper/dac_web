1a5677168ac11bee7259c144649c83d02a7664f2
MIZiper <miziper@163.com>
2025-11-29 23:16:16 +0100

    tiny mpl

---
+++ dac-frontend/src/lib/MplCanvas.svelte
@@ -0,0 +1,31 @@
+<div bind:this={figure} id="figure">
+
+</div>
+
+<script lang="ts">
+    let {mplSite} = $props();
+
+    let figure;
+
+    function initMpl() {
+        ['mpl.css'].forEach((file) => {
+            const link = document.createElement('link');
+            link.rel = 'stylesheet';
+            link.href = `${mplSite}/static/css/${file}`;
+            document.head.appendChild(link);
+        });
+
+        function ondownload(figure, format) {
+            window.open(`${mplSite}/${figure.id}/download.${format}`, '_blank');
+        };
+
+        const script = document.createElement('script');
+        script.src = `${mplSite}/static/js/mpl.js`;
+        script.onload = () => {
+            // @ts-ignore
+            window.mpl.figure(figure);
+        };
+        document.body.appendChild(script);
+    }
+
+</script>
