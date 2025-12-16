c17e2461a0d8337475c1d5db0e1e9d688f590269
MIZiper <miziper@163.com>
2025-12-16 21:14:07 +0100

    import a yaml editor

---
+++ dac-frontend/src/lib/YamlEditor.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { yaml } from "@codemirror/lang-yaml";
+    import CodeMirror from "svelte-codemirror-editor";
+
+    let value = "";
+</script>
+
+<CodeMirror bind:value lang={yaml()} />
--- dac-frontend/src/router.ts
+++ dac-frontend/src/router.ts
@@ -1,8 +1,9 @@
 import { createRouter } from "sv-router";
 import HomePage from "./pages/HomePage.svelte";
 import MainPage from "./pages/MainPage.svelte";
+import YamlEditor from "./lib/YamlEditor.svelte";
 
 export const { p, navigate, isActive, route } = createRouter({
     '/': HomePage,
-    '/dev-test': MainPage,
+    '/dev-test': YamlEditor,
 })
