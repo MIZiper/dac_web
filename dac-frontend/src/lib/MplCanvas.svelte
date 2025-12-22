0b8bd18f08af6f7f13b6ee0c6fa77c44449dbfb5
MIZiper <miziper@163.com>
2025-12-22 15:30:30 +0100

    mpl canvas adaptation

--- dac-frontend/src/lib/MplCanvas.svelte
+++ dac-frontend/src/lib/MplCanvas.svelte
@@ -1,38 +1,143 @@
-<div id="figure">
-
-</div>
-
 <script lang="ts">
-    let {mplSite} = $props();
+    import { Icon } from "@sveltestrap/sveltestrap";
+    import { onMount } from "svelte";
 
-    let figure: any;
+    let { mplSite } = $props();
+
+    // const api_mpl = `${mplSite}/api/mpl`;
+    // const app_mpl = `${mplSite}/app/mpl`;
+    const api_mpl = `/mpl`; // for dev
+    const app_mpl = `/mpl`; // for dev
+
+    let figure: any = $state();
+    let isFullscreen: boolean = $state(false);
+    const FIG_NUM = 1;
 
     function initMpl() {
-        ['mpl.css'].forEach((file) => {
-            const link = document.createElement('link');
-            link.rel = 'stylesheet';
-            link.href = `${mplSite}/static/css/${file}`;
+        ["mpl.css"].forEach((file) => {
+            const link = document.createElement("link");
+            link.rel = "stylesheet";
+            link.href = `${api_mpl}/_static/css/${file}`;
             document.head.appendChild(link);
         });
 
         function ondownload(figure, format) {
-            window.open(`${mplSite}/${figure.id}/download.${format}`, '_blank');
-        };
+            window.open(`${app_mpl}/${figure.id}/download.${format}`, "_blank");
+        }
 
-        const script = document.createElement('script');
-        script.src = `${mplSite}/static/js/mpl.js`;
+        const script = document.createElement("script");
+        script.src = `${api_mpl}/js/mpl.js`;
         script.onload = () => {
-            // @ts-ignore
-            window.mpl.figure(figure);
+            let websocket_type = window.mpl.get_websocket_type();
+            let websocket = new websocket_type(`${app_mpl}/${FIG_NUM}/ws`);
+
+            figure = new window.mpl.figure(
+                FIG_NUM,
+                websocket,
+                ondownload,
+                document.getElementById("figure"),
+            );
+            const widgetImages = document.querySelectorAll(
+                "button.mpl-widget img",
+            );
+            widgetImages.forEach((img) => {
+                const fname = img.src.split("/").pop();
+                const fname_2x = img.srcset.split("/").pop();
+                img.src = `${api_mpl}/_images/${fname}`;
+                img.srcset = `${api_mpl}/_images/${fname_2x}`;
+            });
         };
         document.body.appendChild(script);
+        window.addEventListener("resize", autoScaleCanvas);
+    }
+    function autoScaleCanvas() {
+        if (!figure) return;
+
+        if (isFullscreen) {
+            figure._resize_canvas(
+                window.innerWidth,
+                window.innerHeight * 0.9,
+                true,
+            );
+        } else {
+            figure._resize_canvas(
+                window.innerWidth * 0.66,
+                window.innerHeight * 0.7,
+                true,
+            );
+        }
+    }
+    function toggleFullscreen() {
+        /*
+        // https://stackoverflow.com/a/59499501
+        var doc = window.document;
+        var docEl = doc.documentElement;
+
+        // Request full screen
+        var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+
+        // Exit full screen
+        var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+        if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+            requestFullScreen.call(docEl);
+        }
+        else {
+            cancelFullScreen.call(doc);
+        }
+        */
+        isFullscreen = !isFullscreen;
+        autoScaleCanvas();
     }
 
+    onMount(initMpl);
 </script>
 
+<div id="mpl-container" class={isFullscreen ? "fullscreen-component" : ""}>
+    <div id="fullscreen-btn">
+        <Icon
+            onclick={toggleFullscreen}
+            name={isFullscreen ? "fullscreen-exit" : "arrows-fullscreen"}
+            title="Toggle fullscreen"
+        />
+    </div>
+    {#if !isFullscreen && figure}
+        <div id="autoscale-btn">
+            <Icon
+                onclick={autoScaleCanvas}
+                name="arrow-clockwise"
+                title="Auto resize"
+            />
+        </div>
+    {/if}
+    <div id="figure"></div>
+</div>
+
+<!-- <img src="https://placehold.co/1000x600?text=Loading+matplotlib" /> -->
+
 <style>
-    #figure {
-        min-height: 600px;
-        min-width: 960px;
+    #mpl-container {
+        position: relative;
+    }
+    #fullscreen-btn {
+        position: absolute;
+        top: 4px;
+        right: 8px;
+    }
+    #autoscale-btn {
+        position: absolute;
+        top: 4px;
+        left: 8px;
+    }
+    #fullscreen-btn:hover,
+    #autoscale-btn:hover {
+        color: orange;
+        cursor: pointer;
+    }
+    #mpl-container.fullscreen-component {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        z-index: 9999;
     }
 </style>
--- dac-frontend/vite.config.ts
+++ dac-frontend/vite.config.ts
@@ -4,4 +4,24 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      },
+      '/app': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        ws: true,
+      },
+      '/mpl': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/mpl/, ''),
+      },
+
+    }
+  }
 })
