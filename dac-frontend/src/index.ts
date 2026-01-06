6e3e116aeee02847f71b747594bf82439c463cd2
MIZiper <miziper@163.com>
2026-01-06 11:39:59 +0100

    export a little more

--- dac-frontend/src/index.ts
+++ dac-frontend/src/index.ts
@@ -1,1 +1,5 @@
+export { navTeleport } from "./utils/NavibarSnippet.svelte";
+export { p, navigate, isActive, route } from "./router";
+
+export {default as App} from "./App.svelte";
 export {default as MainPage} from "./pages/MainPage.svelte";
