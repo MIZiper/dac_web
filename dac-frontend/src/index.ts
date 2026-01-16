be291ac50b83d066caea9a6c1cc6dc39157f5a7d
MIZiper <miziper@163.com>
2026-01-16 10:21:23 +0100

    simple export

--- dac-frontend/src/index.ts
+++ dac-frontend/src/index.ts
@@ -1,3 +1,4 @@
 export { navTeleport } from "./utils/NavibarSnippet.svelte";
+export { taskHolder, type TaskBaseProps } from "./tasks/TaskRouter.svelte";
 
 export {default as MainPage} from "./pages/MainPage.svelte";
