752aaf684eecd2564d575235fda3efcd7913000c
MIZiper <miziper@163.com>
2026-05-15 00:06:58 +0200

    export for downstream

--- dac-frontend/src/index.ts
+++ dac-frontend/src/index.ts
@@ -2,3 +2,5 @@ export { navTeleport } from "./utils/NavibarSnippet.svelte";
 export { taskHolder, type TaskBaseProps } from "./tasks/TaskRouter.svelte";
 
 export {default as MainPage} from "./pages/MainPage.svelte";
+export {default as HomePage} from "./pages/HomePage.svelte";
+export { keycloakService } from "./utils/KeycloakService.svelte";
